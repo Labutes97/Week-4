306987d9d4610885cb4ee278fa2def9dfdc3cfd1
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-11 15:15:06 +0100

    exercise 5 (left)

---
+++ 4_9_exercises/exercise_5.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.Screen()
+tess = turtle.Turtle()
+
+# Speed up to maximum
+tess.speed(0)
+
+for step in range(100):
+    tess.forward(step * 5)
+    tess.right(90)
+
+screen.exitonclick()
