c8461806b26beb9f9d624eef21d9525835587eb3
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-19 13:03:13 +0100

    exercise 1 - with input check

--- 4_17_exercises/exercise_1.py
+++ 4_17_exercises/exercise_1.py
@@ -2,13 +2,21 @@ directions = ["N", "E", "S", "W"]
 
 
 def turn_clockwise(direction):
-     current_direction = directions.index(direction)
-     return directions[(current_direction + 1) % len(directions)]
+    if direction not in directions:
+        return None
+
+    current_direction = directions.index(direction)
+    return directions[(current_direction + 1) % len(directions)]
 
 
 def turn_counter_clockwise(direction):
+    if direction not in directions:
+        return None
+
     current_direction = directions.index(direction)
     return directions[(current_direction - 1) % len(directions)]
 
 
-print(turn_clockwise("E") == "SE")
+# tests
+print(turn_clockwise("E") == "S")
+print(turn_clockwise("R") is None)
